5991849f7ddd5d4d2ef1b169c8bb01c86ddbf424
Mo2men-dev <momensameh5@gmail.com>
2025-07-25 00:55:18 +0300

    feat(editor): added defaultExtensions prop to config default extensions for editor

--- src/types/components.ts
+++ src/types/components.ts
@@ -1,6 +1,8 @@
 import type { Extension } from "@tiptap/core";
+import type { DefaultExtensions } from "./extensions";
 
 export interface NoqtaEditorProps {
 	initialContent?: string;
 	extensions?: Extension[];
+	defaultExtensionsConfig?: DefaultExtensions;
 }
