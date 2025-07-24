6ecce31d9e824988050789630a4ef4b40f0a50b0
Mo2men-dev <momensameh5@gmail.com>
2025-07-24 23:37:24 +0300

    chore(editor): refactor NoqtaEditor to use NoqtaEditorProps interface for props

--- src/components/NoqtaEditor.tsx
+++ src/components/NoqtaEditor.tsx
@@ -1,17 +1,15 @@
-import { Extension } from "@tiptap/core";
 import { EditorContent, useEditor } from "@tiptap/react";
 import StarterKit from "@tiptap/starter-kit";
 
-function NoqtaEditor({
-	initialContent,
-	extensions,
-}: {
-	initialContent?: string;
-	extensions?: Extension[];
-}) {
+import type { NoqtaEditorProps } from "../types/components";
+
+/**
+ * NoqtaEditor is a React component that provides a rich text editor using [Tiptap](https://tiptap.dev/).
+ */
+function NoqtaEditor(props: NoqtaEditorProps) {
 	const editor = useEditor({
-		extensions: [StarterKit, ...(extensions || [])],
-		content: initialContent || "<p>Hello World!</p>",
+		extensions: [StarterKit, ...(props.extensions || [])],
+		content: props.initialContent || "<p>Hello World!</p>",
 	});
 
 	return <EditorContent editor={editor} />;
---
+++ src/types/components.ts
@@ -0,0 +1,6 @@
+import type { Extension } from "@tiptap/core";
+
+export interface NoqtaEditorProps {
+	initialContent?: string;
+	extensions?: Extension[];
+}
