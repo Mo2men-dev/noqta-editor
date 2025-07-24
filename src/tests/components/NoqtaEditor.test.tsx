dc199bb6cef8528c7a5f00e96331224e6ea98168
Mo2men-dev <momensameh5@gmail.com>
2025-07-24 04:23:32 +0300

    feat(editor): added NoqtaEditor component

---
+++ src/tests/components/NoqtaEditor.test.tsx
@@ -0,0 +1,12 @@
+import { NoqtaEditor } from "../../../src";
+import { render, screen } from "@testing-library/react";
+
+describe("NoqtaEditor", () => {
+	it("renders without crashing", () => {
+		render(<NoqtaEditor />);
+		const editor = screen.getByRole("textbox");
+		expect(editor).toBeInTheDocument();
+		expect(editor).toHaveAttribute("contenteditable", "true");
+		expect(editor).toHaveClass("ProseMirror");
+	});
+});
